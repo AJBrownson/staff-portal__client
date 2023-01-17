975a208a6c94608b7318f8a9093c356adde11828
Anietie Brownson <anni.brownson@gmail.com>
2023-01-17 18:16:32 +0100

    added QR reader

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -1,17 +1,18 @@
 import { useState } from 'react'
 import axios from 'axios'
 import './styles.css'
+import { QrReader } from 'react-qr-reader';
 
 
 
-const Form = () => {
-  const [name, setName] = useState('')
+const Form = (props) => {
+  const [data, setData] = useState('')
   const [comment, setComment] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault();
     
-    axios.post('http://localhost:5000/api/staff', { name, comment })
+    axios.post('http://localhost:5000/api/staff', { data, comment })
     .then(res => {
       console.log(res.data)
       window.alert('Sign In successful!')
@@ -22,18 +23,30 @@ const Form = () => {
     })
 
     // This clears form input values after submission
-    setName('')
+    setData('')
     setComment('')
   };
 
   return (
     <>
       <form onSubmit={handleSubmit}>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        style={{ width: '100%' }}
+      />
       <label>Name</label>
         <input 
           type="text" 
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={data}
+          onChange={(e) => setData(e.target.value)}
         />
 
       <label>Comment</label>
