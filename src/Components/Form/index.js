198619aed54243aca68da931ae2098f5e6aa4f6f
Anietie Brownson <anni.brownson@gmail.com>
2023-06-01 08:00:13 +0100

    refactor: checking netlify deployment status

--- src/Components/Form/index.js
+++ src/Components/Form/index.js
@@ -13,6 +13,8 @@ const Form = (props) => {
   const [time, setTime] = useState(displayTime);
   const [date, setDate] = useState(displayDate);
 
+  
+
   const handleSubmit = (event) => {
     event.preventDefault();
 
